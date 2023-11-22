10d35ed3e4b837caf8e40be4d1004011db5ca0b9
loutarRanim <ranimloutar1@gmail.com>
2023-11-22 20:31:55 +0100

    first commit

---
+++ src/main/java/com/example/GestionCinemaSpringBoot/GestionCinemaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestionCinemaSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCinemaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCinemaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GestionCinemaSpringBoot/GestionCinemaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionCinemaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCinemaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
